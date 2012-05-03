1413ad091d5e768b110b255a6b0c0f2e25986181
Noah Hoffman <noah.hoffman@gmail.com>
2012-05-03 10:11:42 -0700

    suppress errors using 'head'

--- alnvu/__init__.py
+++ alnvu/__init__.py
@@ -1,10 +1,23 @@
-def _safeint(s):
-    try:
-        return int(s)
-    except ValueError:
-        return s
+import signal
 
-__version__ = "0.1"
-__version_info__ = tuple([_safeint(num) for num in __version__.split('.')])
+__version__ = "0.1.0"
+__version_info__ = (0, 1, 0)
 
+def _exit_on_signal(sig, status=None, message=None):
+    def exit(sig, frame):
+        if message:
+            print >> sys.stderr, message
+        raise SystemExit(status)
+    signal.signal(sig, exit)
 
+def exit_on_sigint(status=1, message="Canceled."):
+    """
+    Set program to exit on SIGINT, with provided status and message.
+    """
+    _exit_on_signal(signal.SIGINT, status, message)
+
+def exit_on_sigpipe(status=None):
+    """
+    Set program to exit on SIGPIPE
+    """
+    _exit_on_signal(signal.SIGPIPE, status)
