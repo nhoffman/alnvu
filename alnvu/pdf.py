909fe8a16e6cbe4100210d5cfb167dcf4cdcaf8a
Noah Hoffman <noah.hoffman@gmail.com>
2012-07-11 22:45:54 -0700

    hide pdf options if reportlab is not available

--- alnvu/pdf.py
+++ alnvu/pdf.py
@@ -1,10 +1,5 @@
 import os
 import sys
-
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch, cm
-from reportlab.lib.pagesizes import letter, landscape, portrait
-
 from itertools import izip_longest, takewhile
 
 def grouper(n, iterable, pad=True):
@@ -22,64 +17,69 @@ def grouper(n, iterable, pad=True):
     else:
         return (takewhile(lambda x: x is not None, c) for c in iterout)
 
-def print_pdf(pages, outfile, fontsize = 7, orientation='portrait',
-              blocks_per_page=1):
+try:
+    from reportlab.pdfgen import canvas
+    from reportlab.lib.units import inch, cm
+    from reportlab.lib.pagesizes import letter, landscape, portrait
+except ImportError:
+    print_pdf = None
+else:
+    def print_pdf(pages, outfile, fontsize = 7, orientation='portrait',
+                  blocks_per_page=1):
 
-    if orientation == 'portrait':
-        pagesize = portrait(letter)
-    elif orientation == 'landscape':
-        pagesize = landscape(letter)
+        if orientation == 'portrait':
+            pagesize = portrait(letter)
+        elif orientation == 'landscape':
+            pagesize = landscape(letter)
 
-    #precalculate some basics
-    top_margin = pagesize[1] - .75*inch
-    bottom_margin = 0.75*inch
-    left_margin = 0.5*inch
-    right_margin = pagesize[0] - 0.5*inch
-    frame_width = right_margin - left_margin
+        #precalculate some basics
+        top_margin = pagesize[1] - .75*inch
+        bottom_margin = 0.75*inch
+        left_margin = 0.5*inch
+        right_margin = pagesize[0] - 0.5*inch
+        frame_width = right_margin - left_margin
 
-    top_margin_offset = 0
+        top_margin_offset = 0
 
-    document_font = 'Courier'
+        document_font = 'Courier'
 
-    def drawPageFrame(canv):
+        def drawPageFrame(canv):
 
-        # pagecount = len(pages)
-        pagecount = len(pageIter)
-        pg = canv.getPageNumber()
-        ofn = os.path.split(outfile)[-1]
+            # pagecount = len(pages)
+            pagecount = len(pageIter)
+            pg = canv.getPageNumber()
+            ofn = os.path.split(outfile)[-1]
 
-        page_center = 0.5 * pagesize[0]
+            page_center = 0.5 * pagesize[0]
 
-        canv.drawCentredString(page_center, top_margin + 0.3*inch,
-            '%(ofn)s' % locals())
+            canv.drawCentredString(page_center, top_margin + 0.3*inch,
+                '%(ofn)s' % locals())
 
-        lineycoord = top_margin + 0.25*inch
-        canv.line(left_margin, lineycoord, right_margin, lineycoord)
-#       canv.setFont(document_font,fontsize)
+            lineycoord = top_margin + 0.25*inch
+            canv.line(left_margin, lineycoord, right_margin, lineycoord)
+    #       canv.setFont(document_font,fontsize)
 
-        canv.drawCentredString(page_center, 0.5*inch,
-            'Page %(pg)s of %(pagecount)s' % locals())
+            canv.drawCentredString(page_center, 0.5*inch,
+                'Page %(pg)s of %(pagecount)s' % locals())
 
-    canv = canvas.Canvas(outfile, invariant=1, pagesize=pagesize)
-    canv.setPageCompression(1)
+        canv = canvas.Canvas(outfile, invariant=1, pagesize=pagesize)
+        canv.setPageCompression(1)
 
-    pageIter = list(grouper(blocks_per_page, pages, pad=False))
+        pageIter = list(grouper(blocks_per_page, pages, pad=False))
 
-    for pagegroup in pageIter:
-        drawPageFrame(canv)
-        canv.setFont(document_font, fontsize)
-        tx = canv.beginText(left_margin, top_margin - top_margin_offset)
+        for pagegroup in pageIter:
+            drawPageFrame(canv)
+            canv.setFont(document_font, fontsize)
+            tx = canv.beginText(left_margin, top_margin - top_margin_offset)
 
-        for i, page in enumerate(pagegroup):
-            for line in page:
-                tx.textLine(line)
-            # separate blocks
-            if blocks_per_page > 1 and i + 1 < blocks_per_page:
-                tx.textLine('')
+            for i, page in enumerate(pagegroup):
+                for line in page:
+                    tx.textLine(line)
+                # separate blocks
+                if blocks_per_page > 1 and i + 1 < blocks_per_page:
+                    tx.textLine('')
 
-        canv.drawText(tx)
-        canv.showPage()
+            canv.drawText(tx)
+            canv.showPage()
 
-
-
-    canv.save()
+        canv.save()
